6741dd6e3331d54df487c09586d5456d406ab938
Kevin Walorz <kmwalorz@gmail.com>
2020-11-24 22:50:37 -0500

    update 11/24/2020

--- main.js
+++ main.js
@@ -51,6 +51,21 @@ const calculateLosses = obj => {
 					return sum + itemValue;							 		});
 		}  else {
 			return "Lucky you!";
-	}
+	};
+}
 
+//Create a function that concatenates n input arrays, where n is variable.
+
+const concat = (...arrays) => arrays.flat();
+
+
+//Your task is to create a Circle constructor that creates a circle with a radius provided by an argument. The circles constructed must have two getters getArea() (PIr^2) and getPerimeter() (2PI*r) which give both respective areas and perimeter (circumference).
+
+class Circle {
+	constructor(radius) {
+		this.radius = radius;
+	}
+	getArea() {return Math.PI * Math.pow(this.radius, 2)};
+	getPerimeter(){return (2*Math.PI) * this.radius};
+}
 

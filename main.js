91446d6f2e3b8972b75aa267cb172bbfadfff899
Kevin Walorz <kmwalorz@gmail.com>
2020-12-08 22:47:06 -0500

    update 12/08/2020

--- main.js
+++ main.js
@@ -206,6 +206,11 @@ const isPositiveDominant = a => {
 		> 0;
 }
 
+//Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+const nextEdge = (side1, side2) => (side1 + side2) -1;
+
+
 
 
 

79b2152085111e3d4f4cc7423e933c727190875b
Kevin Walorz <kmwalorz@gmail.com>
2021-01-23 13:24:16 -0500

    update 01/23/2021

--- main.js
+++ main.js
@@ -486,3 +486,7 @@ function reverseOdd(str) {
     })
     .join(' ');
 }
+
+// #58  Write a function that takes a credit card number and only displays the last four characters. The rest of the card number must be replaced by ************.
+
+const cardHide = card => card.slice(-4).padStart(card.length, '*');
